8ab2ef229f22e99c29acc34ebbb35e86129879fd
Sade C. Johnson <tvibezengineering321@gmail.com>
2023-03-31 00:30:35 -0500

    Added a function that encodes a message into utf8 format, base64 format, and finally into a bytestring.

---
+++ steganography.py
@@ -0,0 +1,16 @@
+import base64 #base64 uses 64 possible values for representing binary data (https://levelup.gitconnected.com/an-introduction-to-base64-encoding-716cdccc58ce)
+
+#This code is from the exercise found here: https://docs.replit.com/tutorials/python/steganography
+
+def encode_message_as_bytestring(message):
+    b64 = message.encode("utf8") #Unicode Transformation Format - 8 bits (UTF-8) encodes the characters as ASCII text aka in human readable / natural form
+    print(b64) #this prints the human readable message that was passed in the exact form that it was passed in as a parameter
+    bytes_ = base64.encodebytes(b64) #encodes the message using base64 encoded data into the binary form
+    print(bytes_) #prints the transformation into binary form
+    bytestring = "".join(["{:08b}".format(x) for x in bytes_]) #Tranformation of the binary form into a sequence of bytes (which allows the data to be stored in a computer)
+    print(bytestring)
+    return bytestring
+
+
+encode_message_as_bytestring("Sade")
+encode_message_as_bytestring("sade")
